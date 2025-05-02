155ccaa3f4b7db8862ffc275ce015c7f3b5b6b98
iBigTestAcc <iBigTestAcc@gmail.com>
2025-05-02 18:17:57 +0700

    Initial working version

---
+++ src/main/java/com/SimpleJavaService/SimpleJaveService/SimpleJaveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.SimpleJavaService.SimpleJaveService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJaveServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJaveServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SimpleJavaService/SimpleJaveService/SimpleJaveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SimpleJavaService.SimpleJaveService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJaveServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
